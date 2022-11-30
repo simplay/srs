52ccbe84526b7e04b8a8306b2409cefac5d82484
simplay <silent.simplay@gmail.com>
2022-11-30 11:34:09 +0100

    Link usability testing files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,22 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+        <div className={styles.buttons}>
+          <h2>
+            <a style={{color: 'white', fontWeight: 'bold'}} target="_blank" href={ require("/101e672a-8bf5-4fc4-ad0d-413d687267f5.pdf").default } download>Download SRS Usability Test PDF</a>
+          </h2>
+        </div>
+        <div className={styles.buttons}>
+          <h2>
+            <a style={{color: 'white', fontWeight: 'bold'}} target="_blank" href={ require("/79e373b0-efeb-4e50-9e81-431487795856.pdf").default } download>Download Protocol SRS Usability Test PDF</a>
+          </h2>
+        </div>
         <div className={styles.buttons}>
           <h2>
             The source-code of this project will be released as soon as SRS's research paper got published.
           </h2>
+
         </div>
       </div>
     </header>
